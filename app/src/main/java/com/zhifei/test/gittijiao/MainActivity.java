39fa0fca523477e8a90fe0912b3f818f635286d4
changzhifei <2629956255@qq.com>
2017-11-30 20:46:11 +0800

    //这是我第一次提交的文件信息

--- app/src/main/java/com/zhifei/test/gittijiao/MainActivity.java
+++ app/src/main/java/com/zhifei/test/gittijiao/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ///这是我第一次提交文件
+        ///这是我第一次提交文件信息
     }
 }
