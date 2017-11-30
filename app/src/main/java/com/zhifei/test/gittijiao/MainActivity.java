8dd462f9597158dfb1e1d25ab01eb28dd80eb974
changzhifei <2629956255@qq.com>
2017-11-30 20:52:32 +0800

    //这是我第一次提交的文件信息 //新加入的信息 //又加入了一些信息

--- app/src/main/java/com/zhifei/test/gittijiao/MainActivity.java
+++ app/src/main/java/com/zhifei/test/gittijiao/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ///这是我第一次提交文件信息
         //新添加的信息
+        //再次添加
         //这是我新修改的一些东西，希望能够下载
     }
 }
