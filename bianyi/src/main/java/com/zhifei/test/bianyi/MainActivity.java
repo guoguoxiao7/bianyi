0d029143d063870ec5c1b0cf3fba2545fc279a92
guoguoxiao7 <30652406+guoguoxiao7@users.noreply.github.com>
2017-12-01 15:42:32 +0800

    Update MainActivity.java

--- bianyi/src/main/java/com/zhifei/test/bianyi/MainActivity.java
+++ bianyi/src/main/java/com/zhifei/test/bianyi/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //这是新版的插件
+//  这是新加入的内容
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
